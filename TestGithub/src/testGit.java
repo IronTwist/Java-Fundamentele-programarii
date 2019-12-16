875e1d726a866c85d939a753da4816cd93f47fd6
IronTwist <47633722+IronTwist@users.noreply.github.com>
2019-12-16 17:01:09 +0200

    Update testGit.java

--- TestGithub/src/testGit.java
+++ TestGithub/src/testGit.java
@@ -4,6 +4,7 @@ public class testGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Schimbat pe myria");
+		System.out.println("modificat pe lenovo");
 	}
 
 }
